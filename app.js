2355fa8131274533b6acc646f70c7752cdb71301
iethicalhackerhari <iethicalhackerhari@gmail.com>
2022-04-22 18:44:51 +0530

    intro-added

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const visual = document.querySelector('.visual')
 const formClose = document.querySelector('.close-form')
 const saveMusic = document.querySelector('.save-music') //form save btn
 
-
 saveMusicbtn.addEventListener('click', () => {
     const musicName = saveName.value;
     if (musicName == '') {
@@ -48,12 +47,16 @@ resetBtn.addEventListener('click', () => {
         music.pop()
     }
     saveBtn.style.display = 'none'
+    playBtn.style.display = 'none'
+    resetBtn.style.display = 'none'
 
     document.body.style.backgroundColor = "white"
 })
 window.addEventListener('load', () => {
     if (music.length == 0) {
         saveBtn.style.display = 'none'
+        playBtn.style.display = 'none'
+        resetBtn.style.display = 'none'
     }
     const pads = document.querySelectorAll('.pads div')
     const colors = [
@@ -65,18 +68,30 @@ window.addEventListener('load', () => {
         "#ff0015"
     ]
     pads.forEach((pad, index) => {
-        pad.addEventListener('click', () => {
+        pad.addEventListener('click', (e) => {
             if (music.length < 0) {
+                playBtn.style.display = 'none'
+                resetBtn.style.display = 'none'
                 saveBtn.style.display = 'none'
             } else {
                 saveBtn.style.display = 'flex'
+                playBtn.style.display = 'flex'
+                resetBtn.style.display = 'flex'
             }
+            console.log(e.target.classList[0]);
             console.log(sounds);
             sounds[index].currentTime = 0;
             sounds[index].play()
             // console.log(sounds[index].getAttribute('id'));
             music.push(sounds[index].getAttribute('id'))
             console.log(music);
+            const pad = document.querySelector(`.${e.target.classList[0]}`)
+            pad.style.animation=`${e.target.classList[0]} 0.2s ease`
+            pad.addEventListener('animationend',()=>{
+                pad.style.animation=''
+
+            })
+            console.log(pad);
             createVisual(index);
 
 
@@ -116,10 +131,17 @@ const temp = { ...localStorage }
 console.log(temp);
 const items = Object.entries(temp)
 console.log(items);
+
+if(items.length>0)
+    {
+        document.querySelector('.intro').style.display='none'
+    }
 function showSavedMusics() {
-    if (items.length == 0) { document.querySelector('.saved').style.display = 'none' }
+    if (items.length == 0) { 
+        
+        document.querySelector('.saved').style.display = 'none' }
     items.forEach(item => {
-        const newLi = document.createElement('tr')
+      const newLi = document.createElement('tr')
         newLi.classList.add('new-li')
         newLi.classList.add('saved-item')
         newLi.innerHTML = `
@@ -127,11 +149,7 @@ function showSavedMusics() {
         <td><i class="fas fa-play" id="${item[0]}"></i></td>
         <td><i class="fas fa-trash delMusic text-danger"  id="${item[0]}"></td>
       `
-        // newLi.innerHTML=`
-        // <div class="saved-item d-flex">
-        //     <p>${item[0]}</p>
-        //     <i class="fas fa-play" id="${item[0]}"></i>
-        // </div>`
+       
 
         document.querySelector('.tbody').appendChild(newLi);
 
@@ -142,8 +160,10 @@ const reduced = []
 
 // Document EventListner ................................................
 document.addEventListener('click', async (e) => {
+    
 //updateSavedMusics()
     // console.log(e.target.id);
+    document.querySelector('.intro').style.display='none'
     // console.log(e.target.classList.contains('fas'));
     if (e.target.classList.contains('fa-play')) {
         // $('#refresh-div').load(location.pathname + ' #refresh-div')
@@ -186,6 +206,7 @@ document.addEventListener('click', async (e) => {
 showSavedMusics()
 
 function updateSavedMusics() {
+    
     const p = document.querySelectorAll('.fa-play')
     const existing = []
     p.forEach(p =>{
@@ -194,7 +215,10 @@ function updateSavedMusics() {
     console.log(existing);
     const tempUpdate = { ...localStorage }
     const itemsUpdate = Object.entries(tempUpdate)
-    if (itemsUpdate.length == 0) { document.querySelector('.saved').style.display = 'none' }
+    if (itemsUpdate.length == 0) { document.querySelector('.saved').style.display = 'none'  }
+    else{
+        document.querySelector('.intro').style.display='none'
+    }
     const updatedMusics = []
     itemsUpdate.forEach((item,index) => {
         updatedMusics.push(item[0])
